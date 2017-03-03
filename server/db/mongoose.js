79e8755be7408936b2a80060a02f4f90ec71ba19
John Steam <teamsteamdev@gmail.com>
2017-03-03 16:58:28 -0600

    Add log/ to .gitignore

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,10 +2,11 @@ let mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
 
-console.log(JSON.stringify(process.env, undefined, 2))
+let path = process.env.PATH
+console.log('process.env.PATH contains "heroku"', /heroku/.test(path))
 
 const dbUri = {
-  mlabUri: 'mongodb://todoApi:&C2*cW42y22X@ds011890.mlab.com:11890/teamsteam-node-todo-api',
+  mlabUri: 'mongodb://todoApi:7kPYx2H16O0A@ds011890.mlab.com:11890/teamsteam-node-todo-api',
   localUri: 'mongodb://localhost:27017/TodoApp'
 }
 mongoose.connect( process.env.PORT ? dbUri.mlab : dbUri.localhost)
