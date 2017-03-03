9ba6c5a9ff055d7aca5d5068294e1c75ae72f326
John Steam <teamsteamdev@gmail.com>
2017-03-03 17:09:53 -0600

    Fix db switching in mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,14 +2,15 @@ let mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
 
-let path = process.env.PATH
-console.log('process.env.PATH contains "heroku"', /heroku/.test(path))
+const path = process.env.PATH
+const onHeroku = (path) => /heroku/.test(path)
+console.log('process.env.PATH contains "heroku"', onHeroku(path))
 
 const dbUri = {
-  mlabUri: 'mongodb://todoApi:7kPYx2H16O0A@ds011890.mlab.com:11890/teamsteam-node-todo-api',
-  localUri: 'mongodb://localhost:27017/TodoApp'
+  mLab: 'mongodb://todoApi:7kPYx2H16O0A@ds011890.mlab.com:11890/teamsteam-node-todo-api',
+  local: 'mongodb://localhost:27017/TodoApp'
 }
-mongoose.connect( process.env.PORT ? dbUri.mlab : dbUri.localhost)
+mongoose.connect(onHeroku(path) ? dbUri.mLab : dbUri.local)
 
 module.exports = {
   mongoose
