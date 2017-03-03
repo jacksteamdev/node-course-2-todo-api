9cdeae3b001f785498d21ba6993e82776a3dec5a
John Steam <teamsteamdev@gmail.com>
2017-03-03 17:22:42 -0600

    Refactoring dbUris logic in mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,16 +2,17 @@ let mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
 
-const path = process.env.PATH
-const onHeroku = (path) => /heroku/.test(path)
-console.log('process.env.PATH contains "heroku"', onHeroku(path))
-
 const dbUri = {
   mLab: 'mongodb://todoApi:7kPYx2H16O0A@ds011890.mlab.com:11890/teamsteam-node-todo-api',
   local: 'mongodb://localhost:27017/TodoApp'
 }
-mongoose.connect(onHeroku(path) ? dbUri.mLab : dbUri.local)
 
-module.exports = {
-  mongoose
-}
+// TODO: fix this logic later in the course
+const path = process.env.PATH
+const onHeroku = (path) => /heroku/.test(path)
+
+process.env.MONGODB_URI = onHeroku(path) ? dbUri.mLab : dbUri.local
+
+mongoose.connect(process.env.MONGODB_URI)
+
+module.exports = {mongoose}
