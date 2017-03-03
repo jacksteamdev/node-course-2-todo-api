51970e6283d1fa3a841b1696f12e8072b1466765
John Steam <teamsteamdev@gmail.com>
2017-03-03 16:45:33 -0600

    Debuggin dbUri in mongoose.js for heroku deployment

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,11 +2,13 @@ let mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
 
+console.log(JSON.stringify(process.env, undefined, 2))
+
 const dbUri = {
   mlabUri: 'mongodb://todoApi:&C2*cW42y22X@ds011890.mlab.com:11890/teamsteam-node-todo-api',
   localUri: 'mongodb://localhost:27017/TodoApp'
 }
-mongoose.connect(dbUri.mlab || dbUri.local)
+mongoose.connect( process.env.PORT ? db.mlab : db.localhost)
 
 module.exports = {
   mongoose
