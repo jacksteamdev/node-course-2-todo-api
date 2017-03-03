4836d4c50e38ad206f10667d5572abd960d4e742
John Steam <teamsteamdev@gmail.com>
2017-03-03 16:27:17 -0600

    Change db uris

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,7 +6,7 @@ const dbUri = {
   mlabUri: 'mongodb://todoApi:&C2*cW42y22X@ds011890.mlab.com:11890/teamsteam-node-todo-api',
   localUri: 'mongodb://localhost:27017/TodoApp'
 }
-mongoose.connect(dbUri.local || dbUri.mlab)
+mongoose.connect(dbUri.mlab || dbUri.local)
 
 module.exports = {
   mongoose
