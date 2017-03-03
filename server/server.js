b15315fffb5c69808dd159da2bf6ef68e16c3fb9
John Steam <teamsteamdev@gmail.com>
2017-03-03 15:21:26 -0600

    Churn /todos/:id route

--- server/server.js
+++ server/server.js
@@ -30,34 +30,23 @@ app.get('/todos', (req, res) => {
   })
 })
 
-// TODO: GET /todos/12345
+// CHALLENGE: GET /todos/:id
 app.get('/todos/:id', (req, res) => {
   let id = req.params.id
 
-  // Validate id using isValid
   if (!ObjectID.isValid(id)) {
-    // 404 - send back empty body
     console.log('Id is not valid, send 404')
     return res.status(404).send()
   }
 
-  // findById
   Todo.findById(id).then((todo) => {
-    // success
-    if (todo) {
-      // if todo - send it back
-      console.log('Todo found', JSON.stringify(todo, undefined, 2))
-      res.send({todo})
-    } else {
-      // if no todo - send back 404 with empty body
-      console.log('Todo not found')
-      res.status(404).send()
+    if (!todo) {
+      return res.status(404).send()
     }
+    res.send({todo})
   }).catch((err) => {
-    // error
-      // 400 - and send empty body back
     console.log('Promise.catch error', err)
-    res.send(400).send(err)
+    res.status(400).send()
   })
 })
 
