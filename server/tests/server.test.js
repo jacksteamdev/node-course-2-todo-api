57ce3607d3ec3ff4e4de505410ce93d88a19eb7c
John Steam <teamsteamdev@gmail.com>
2017-03-06 10:56:17 -0600

    test DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -89,16 +89,56 @@ describe('GET /todos/:?', () => {
 
   it('should return 404 if todo not found', (done) => {
     let hexId = new ObjectID().toHexString()
+
     request(app)
       .get(`/todos/${hexId}`)
       .expect(404)
       .end(done)
   })
 
-  it('should return 404 for non-ObjectIds', (done) => {
+  it('should return 400 for non-ObjectIds', (done) => {
     request(app)
       .get('/todos/123')
+      .expect(400)
+      .end(done)
+  })
+})
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    let hexId = todos[1]._id.toHexString()
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId)
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err)
+        }
+
+        Todo.findById(hexId).then((todo) => {
+          expect(todo).toNotExist()
+          done()
+        }).catch((err) => done(err))
+      })
+  })
+
+  it('should return 404 if todo not found', (done) => {
+    let hexId = new ObjectID().toHexString()
+
+    request(app)
+      .delete(`/todos/${hexId}`)
       .expect(404)
+      .end(done)
+  })
+
+  it('should return 400 if object id is invalid', (done) => {
+    request(app)
+      .delete('/todos/123')
+      .expect(400)
       .end(done)
   })
 })
