88f5b0f8f4d69fd7d18fa00a4c84d7ceca5d39f5
John Steam <teamsteamdev@gmail.com>
2017-03-10 10:58:49 -0600

    Add POST /users/login tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -239,3 +239,48 @@ describe('POST /users', () => {
       .end(done)
   })
 })
+
+describe('POST /users/login', () => {
+  it('should login user and return auth token', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toExist()
+      })
+      .then((res) => {
+        return User.findById(users[1]._id)
+          .then((user) => {
+            expect(user.tokens[0]).toInclude({
+              access: 'auth',
+              token: res.headers['x-auth']
+            })
+            done()
+          })
+      })
+      .catch((err) => done(err))
+  })
+
+  it('should reject invalid login', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: '123'
+      })
+      .expect(400)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toNotExist()
+        return User.findById(users[1]._id)
+          .then((user) => {
+            expect(user.tokens.length).toBe(0)
+            done()
+          })
+      })
+      .catch((err) => done(err))
+  })
+})
